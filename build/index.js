2d486b9199d339555d58b18fa9d0c8ac0aa935ad
Korneel Eeckhout <korneel@pienter.tech>
2024-03-13 21:38:25 +0100

    test

--- build/index.js
+++ build/index.js
@@ -1,95 +1,13 @@
 import render from "@heml/render";
 import inline from "@heml/inline";
 import validate from "@heml/validate";
+import parse from "@heml/parse";
 import utils from "@heml/utils";
 import byteLength from "byte-length";
 import beautify from "js-beautify";
 import lodash from "lodash";
 import * as coreElements from "@heml/elements";
-import { load } from "cheerio";
-import randomString from "crypto-random-string";
-import htmlTags from "html-tags";
-import selfClosingHtmlTags from "html-tags/void.js";
 
-const wrappingHtmlTags = lodash.difference(htmlTags, selfClosingHtmlTags);
-
-function parse(contents, options = {}) {
-  const { elements = [], cheerio: cheerioOptions = {} } = options;
-  const $ = load(contents, {
-    xmlMode: true,
-    lowerCaseTags: true,
-    decodeEntities: false,
-    ...cheerioOptions,
-  });
-  $.findNodes = function (q) {
-        console.log({q});
-        return $('heml').toNodes();
-    return $(Array.isArray(q) ? q.join(",") : q)
-      .not("[heml-ignore]")
-      .toNodes();
-  };
-  $.prototype.toNodes = function () {
-    return this.toArray().map((node) => $(node));
-  };
-  const selfClosingTags = [
-    ...selfClosingHtmlTags,
-    ...elements
-      .filter((element) => element.children === false)
-      .map(({ tagName }) => tagName),
-  ];
-  const wrappingTags = [
-    ...wrappingHtmlTags,
-    ...elements
-      .filter((element) => element.children !== false)
-      .map(({ tagName }) => tagName),
-  ];
-
-  const $selfClosingNodes = $.findNodes(selfClosingTags).reverse();
-  const $wrappingNodes = $.findNodes(wrappingTags).reverse();
-
-  /** Move contents from self wrapping tags outside of itself */
-  $selfClosingNodes.forEach(($node) => {
-    $node.after($node.html());
-    $node.html("");
-  });
-
-  /** ensure that all wrapping tags have at least a zero-width, non-joining character */
-  $wrappingNodes.forEach(($node) => {
-    if ($node.html().length === 0) {
-      $node.html(" ");
-    }
-  });
-
-  /** try for head, fallback to body, then heml */
-  const $head = lodash.first(
-    lodash.compact([
-      ...$("head").toNodes(),
-      ...$("body").toNodes(),
-      ...$("heml").toNodes(),
-    ]),
-  );
-
-  /** move inline styles to a style tag with unique ids so they can be hit by the css processor */
-  if ($head) {
-    const $inlineStyleNodes = $.findNodes(
-      elements.map(({ tagName }) => tagName),
-    ).filter(($node) => !!$node.attr("style"));
-    const inlineCSS = $inlineStyleNodes
-      .map(($node) => {
-        let id = $node.attr("id");
-        const css = $node.attr("style");
-        $node.removeAttr("style");
-        if (!id) {
-          id = `heml-${randomString(5)}`;
-          $node.attr("id", id);
-        }
-        return `#${id} {${css}}`;
-      })
-      .join("\n");
-    $head.append(`<style>${inlineCSS}</style>`);
-  }
-  return $;
-}
 /**
  * renders the given HEML string with the config provided
  * @param  {String} HEML     the heml to render
@@ -105,10 +23,10 @@ async function heml(contents, options = {}) {
   );
 
   /** parse it ✂️ */
-  const $heml = parse(contents, options);
+  const $heml = parse.default(contents, options);
 
   /** validate it 🕵 */
-  const errors = validate($heml, options);
+  const errors = validate.default($heml, options);
   if (validateOption.toLowerCase() === "strict" && errors.length > 0) {
     throw errors[0];
   }
