4e24c18b3a03fbcfc313f893a022dab0904dd135
Korneel Eeckhout <korneel@pienter.tech>
2025-01-14 10:58:58 +0100

    update packages/heml-parse

--- packages/heml-parse/src/index.js
+++ packages/heml-parse/src/index.js
@@ -1,11 +1,13 @@
 import { load } from "cheerio";
-import { difference, compact, first } from "lodash";
-import randomString from "crypto-random-string";
+import lodash from "lodash";
+import cryptoRandomString from "crypto-random-string";
 import htmlTags, { voidHtmlTags } from "html-tags";
+
+const { difference, compact, first } = lodash;
 
 const wrappingHtmlTags = difference(htmlTags, voidHtmlTags);
 
-function parse(contents, options = {}) {
+export default function parse(contents, options = {}) {
   const { elements = [], cheerio: cheerioOptions = {} } = options;
 
   const $ = load(contents, {
@@ -76,7 +78,7 @@ function parse(contents, options = {}) {
         $node.removeAttr("style");
 
         if (!id) {
-          id = `heml-${randomString(5)}`;
+          id = `heml-${cryptoRandomString({ length: 5 })}`;
           $node.attr("id", id);
         }
 
@@ -89,5 +91,3 @@ function parse(contents, options = {}) {
 
   return $;
 }
-
-export default parse;
